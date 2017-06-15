464e53866acbb4fa13b9c63ba33e7b6d89aa5e6b
sherinann <sherinannthomas1@gmail.com>
2017-06-15 18:48:44 +0530

    DSL  window

---
+++ tic-tac-toe/tic-tac-toe/main.c
@@ -0,0 +1,61 @@
+//
+//  main.c
+//  tic-tac-toe
+//
+//  Created by Sherin Ann Thomas on 15/06/17.
+//  Copyright © 2017 Sherin Ann Thomas. All rights reserved.
+//
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+const int WIDTH=800,HEIGHT=600;
+
+int main(int argc, const char * argv[]) {
+    
+    SDL_Surface *imageSurface;
+    SDL_Surface *windowSurface;
+    
+    if( SDL_Init(SDL_INIT_EVERYTHING)<0){
+        printf("error %s",SDL_GetError());
+    }
+    
+    SDL_Window *window= SDL_CreateWindow("Tic Tac Toe", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    windowSurface= SDL_GetWindowSurface(window);
+    
+    if(NULL==window){
+        printf("error %s",SDL_GetError());
+        return EXIT_FAILURE;
+    }
+   
+    SDL_Event windowEvent;
+    imageSurface = SDL_LoadBMP("tic.bmp");
+    if(NULL==imageSurface){
+        printf("image could not be loaded %s",SDL_GetError());
+        
+    }
+    
+    while (1) {
+        if(SDL_PollEvent(& windowEvent)){
+            
+            if(windowEvent.type==SDL_QUIT){
+                break;
+            }
+        }
+        
+        SDL_BlitSurface(imageSurface,NULL,windowSurface,NULL);
+        SDL_UpdateWindowSurface(window);
+    }
+        
+    SDL_FreeSurface(imageSurface);
+    SDL_FreeSurface(windowSurface);
+    
+    imageSurface=NULL;
+    windowSurface=NULL;
+        
+        
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return EXIT_SUCCESS;
+}
