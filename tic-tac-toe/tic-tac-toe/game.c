73c07bae53b5ac295247306740bd87ab32561434
sherinann <sherinannthomas1@gmail.com>
2017-06-23 19:35:19 +0530

    game logic complete

--- tic-tac-toe/tic-tac-toe/game.c
+++ tic-tac-toe/tic-tac-toe/game.c
@@ -24,6 +24,10 @@ int board[3][3];
 int p[2];
 
 
+void printMove(int posi,int posj){
+	  printf("\nmove :%d %d",posi,posj);
+
+}
 
 int checkwin(int mark){
     int i,j,count;
@@ -83,11 +87,101 @@ int checkwin(int mark){
 
 
 
+int checkCompAboutTowin(mark){
+	int i,j,count;
+	for(i=0;i<3;i++){
+		count=0;
+		for(j=0;j<3;j++){
+			if(board[i][j]==mark){
+				count++;
+			}
+
+		}
+		if(count==2){
+			for(j=0;j<3;j++){
+				if(board[i][j]==UNSET){
+					board[i][j]=mark;
+						printMove(i,j);
+					return 1;
+					}
+
+				}
+				
+
+		}
+	}
+
+for(j=0;j<3;j++){
+		count=0;
+		for(i=0;i<3;i++){
+			if(board[i][j]==mark){
+				count++;
+			}
+
+		}
+		if(count==2){
+			for(i=0;i<3;i++){
+				if(board[i][j]==UNSET){
+					board[i][j]=COMPUTER;
+						printMove(i,j);
+				return 1;
+				}
+				
+			}
+
+		}
+	}
+
+	count=0;
+	for(i=0;i<3;i++){
+		if(board[i][i]==mark){
+			count++;
+		}
+	}
+	if(count==2){
+		for(i=0;i<3;i++){
+			if(board[i][i]==UNSET){
+				board[i][i]=COMPUTER;
+					printMove(i,i);
+				return 1;
+			}
+		}
+	}
+
+	count=0;
+	for(i=0;i<3;i++){
+		if(board[i][2-i]==mark){
+			count++;
+		
+		}
+	}
+	if(count==2){
+		for(i=0;i<3;i++){
+			if(board[i][2-i]==UNSET){
+				board[i][2-i]=COMPUTER;
+					printMove(i,2-i);
+				return 1;
+			}
+		}
+
+	}
+
+	return 0;
+}
+
+
 //to make move on behalf of the computer
 int  moveComputer(int mark){
     
-    int i,j,posi,posj,count;
+    int i,j,posi,posj,count,winStatus;
+
 	//need to block
+
+winStatus=checkCompAboutTowin(COMPUTER);
+if(winStatus){
+
+	return 1;
+}
 
 	for(i=0;i<3;i++){
 		count=0;
@@ -102,6 +196,7 @@ int  moveComputer(int mark){
 			for(j=0;j<3;j++){
 				if(board[i][j]==UNSET){
 					board[i][j]=COMPUTER;
+					printMove(i,j);
 					return 1;
 				}
 
@@ -126,6 +221,7 @@ int  moveComputer(int mark){
 			for(i=0;i<3;i++){
 				if(board[i][j]==UNSET){
 					board[i][j]=COMPUTER;
+					printMove(i,j);
 					return 1;
 				}
 
@@ -145,6 +241,7 @@ int  moveComputer(int mark){
 		for(i=0;i<3;i++){
 			if(board[i][i]==UNSET){
 				board[i][i]=COMPUTER;
+				printMove(i,i);
 				return 1;
 			}
 		}
@@ -162,6 +259,7 @@ int  moveComputer(int mark){
 			for(i=0;i<3;i++){
 				if(board[i][2-i]==UNSET){
 					board[i][2-i]=COMPUTER;
+					printMove(i,2-i);
 					return 1;
 				}
 			}
@@ -171,41 +269,47 @@ int  moveComputer(int mark){
     
 	if(board[1][1]==UNSET){
         board[1][1]=COMPUTER;
+		printMove(1,1);
 		return 1;
 
 	}
 
 	if(board[0][0]==UNSET){
 		board[0][0]=COMPUTER;
+		printMove(0,0);
 		return 1;
 
 	}
 
 	if(board[0][2]==UNSET){
 		board[0][2]=COMPUTER;
+		printMove(0,2);
 		return 1;
 
 	}
 	if(board[2][0]==UNSET){
 		board[2][0]=COMPUTER;
+		printMove(2,0);
 		return 1;
 
 	}
 	if(board[2][2]==UNSET){
 		board[2][2]=COMPUTER;
+		printMove(2,2);
 		return 1;
 	}
 	for(i=0;i<3;i++){
 		for(j=0;j<3;j++){
 			if(board[i][j]==UNSET){
 				board[i][j]=COMPUTER;
+				printMove(i,j);
 				return 1;
 			}
 			
 
 		}
 	}
-    printf("move :%d %d",posi,posj);
+  
     return 0;
 }
 
@@ -238,10 +342,10 @@ int main(){
 
 	int i,j,result;
     int moveStatus;
-	char opt='y';
-
-	while(opt=='y'){
-        int counter=0;
+	int opt=1;
+	 int counter=0;
+	while(opt==1){
+       
         
 		 //status 0 if game going on else 1
 		int status=0;
@@ -267,6 +371,10 @@ int main(){
 
 					//computer makes move first. then player. checks for win after each move
 					moveStatus=moveComputer(USER);
+					if(!moveStatus){
+						status=1;
+						break;
+					}
 					result=checkwin(COMPUTER);
 					if(result==1){
 						printf("you lose");
@@ -319,6 +427,10 @@ int main(){
 						}
 					}
                     moveStatus=moveComputer(USER);
+					if(!moveStatus){
+						status=1;
+						break;
+					}
 					result=checkwin(COMPUTER);
 					if(result==1){
 						printf("\nyou lose");
@@ -336,14 +448,15 @@ int main(){
 				}
 
 			}
-			if(endResult==1){
-				printf("its a tie");
-			}
+			
 
 		}
 
-	printf("\nplay again?(y/n)");
-		scanf("%c",&opt);
+if(endResult==1){
+				printf("its a tie");
+	}
+	printf("\nplay again?(1/0)");
+		scanf("%d",&opt);
 		counter++;
 	}
 
